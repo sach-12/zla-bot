85ac6122a70c8cc219da28bae3c57f7d97fccb5e
sach-12 <sachinshankar2010@gmail.com>
2021-08-13 01:51:28 +0530

    finished controller and online commands

--- cogs/comm.py
+++ cogs/comm.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+import datetime
+from cogs.func import func
 
 
 class comm(commands.Cog):
@@ -20,5 +24,71 @@ class comm(commands.Cog):
         await ctx.channel.send("I am help command")
 
 
+    @commands.command(aliases=['online'])
+    async def online_command(self, ctx):
+        await ctx.channel.trigger_typing()
+        url = 'https://laartcc.org/'
+        res = requests.get(url)
+        if(res.status_code == 200):
+            soup = BeautifulSoup(res.content, "html.parser")
+            online = soup.find("div", class_="list-group")
+            controller_list = online.find_all("a")
+            emb = discord.Embed(title="Controllers online in Los Angeles ARTCC", timestamp=datetime.datetime.now())
+            if(len(controller_list) == 0):
+                emb.description = "No controllers online"
+                emb.color = discord.Color.red()
+                await ctx.channel.send(embed=emb)
+                return
+            else:
+                emb.color = discord.Color.green()
+            for controller in controller_list:
+                info = controller.text.strip().split('\n')
+                position = info[0]
+                name = info[1].split('/')[0].strip()
+                profile_link = controller["href"]
+                cid = profile_link.split('/')[-1]
+                online_time = controller["data-title"].strip().split('–')[1].strip()
+                emb.add_field(name=f"{position} - {name} ({cid})",value=f"{online_time} [Controller Profile]({profile_link})", inline=False)
+            await ctx.channel.send(embed=emb)
+        else:
+            emb = discord.Embed(title="Something's wrong", description="Try again after a while", timestamp=datetime.datetime.now(), color=discord.Color.red())
+            await ctx.channel.send(embed=emb)
+
+
+    @commands.command(aliases = ['controller'])
+    async def controller_command(self, ctx, query:str = ''):
+        await ctx.channel.trigger_typing()
+        help_embed = discord.Embed(title="LA ARTCC Controller Information", color=discord.Color.blue())
+        help_embed.add_field(name="zla!controller [initials or CID]", value="Returns information about a specific ZLA controller given the query")
+        if(query == ''):
+            await ctx.channel.send(embed=help_embed)
+            return
+        url = f"https://laartcc.org/controller/{query}"
+        profile = func.profileFromCid(func(self.client), url)
+        if(profile == 404):
+            ret = func.getUrl(func(self.client), query)
+            if(ret == 404):
+                await ctx.channel.send(content="Controller not found on the roster", embed=help_embed)
+                return
+            else:
+                profile = func.profileFromCid(func(self.client), ret)
+                if(profile == 404):
+                    embx = discord.Embed(title="Something's wrong", description="Try again after a while", color=discord.Color.red())
+                    await ctx.channel.send(embed=embx)
+                    return
+        name = f"{profile[0]} - {profile[6]}"
+        rating = profile[1]
+        month = profile[4]
+        month_hours = profile[2]
+        total_hours = profile[3]
+        img_src = profile[5]
+        profile_url = profile[7]
+        emb = discord.Embed(title=name, description=rating, url=profile_url, color=0xff10f0)
+        emb.set_thumbnail(url=img_src)
+        emb.add_field(name=f"Hours - {month}", value=month_hours)
+        emb.add_field(name="Hours - Total", value=total_hours)
+        await ctx.channel.send(embed=emb)
+        
+        
 def setup(client):
     client.add_cog(comm(client))
---
+++ cogs/func.py
@@ -0,0 +1,75 @@
+import discord
+from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+
+
+class func(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.ratings = {
+            "S1": "Student 1",
+            "S2": "Student 2",
+            "S3": "Student 3",
+            "C1": "Controller 1",
+            "C3": "Controller 3",
+            "I1": "Instructor 1"
+        }
+
+
+    def profileFromCid(self, url:str):
+        res = requests.get(url=url)
+        if(res.status_code == 200):
+            soup = BeautifulSoup(res.content, "html.parser")
+            name = soup.find("h3", class_="mb-0 text-truncated").text.strip()
+            qual = soup.find("p", class_="lead").text.strip()
+            rating = self.getRating(qual=qual)
+            totals = soup.find_all("td", class_='"totals"')
+            month_hours = totals[1].text.strip()
+            if(month_hours == ""):
+                month_hours = "0:00"
+            total_hours = totals[-1].text.strip()
+            month_table = soup.find("table", class_="table table-striped table-bordered table-sm stats")
+            month = month_table.findChild("th").text.strip().capitalize()
+            img = soup.find("img", class_="img-fluid rounded img-thumbnail")
+            img_src = img["src"]
+            cid = url.split('/')[-1]
+            return name, rating, month_hours, total_hours, month, img_src, cid, url
+        else:
+            return 404
+
+
+    def getRating(self, qual:str):
+        qual_list = qual.split('/')
+        if(len(qual_list) == 1):
+            if(qual_list[0].strip() in self.ratings):
+                return f"{qual_list[0].strip()} - {self.ratings[qual_list[0].strip()]}"
+            else:
+                return qual_list[0].strip()
+        else:
+            if(qual_list[1].strip() in self.ratings):
+                return f"{qual_list[0].strip()} / {qual_list[1].strip()} - {self.ratings[qual_list[1].strip()]}"
+            else:
+                return qual_list[1].strip()
+
+
+    def getUrl(self, initials:str):
+        url = "https://laartcc.org/roster"
+        res = requests.get(url=url)
+        soup = BeautifulSoup(res.content, "html.parser")
+        roster = soup.find(id='roster')
+        controller_list = roster.findChildren()
+        for controller in controller_list:
+            try:
+                c = (controller.text.strip().split('\n')[2])
+                if(c == initials.upper()):
+                    profile = controller.find("a")
+                    return profile["href"]
+            except:
+                continue
+        return 404
+
+
+def setup(client):
+    client.add_cog(func(client))
